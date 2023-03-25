ea8ce1f785501922783c26d7db82c206b11d07a8
paraskumar09 <125036495+paraskumar09@users.noreply.github.com>
2023-03-25 19:25:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        
+        
+        var button= this.innerHTML;
+       makesound(button);
+
+       
+    });
+
+
+    document.addEventListener("keydown",function(event){
+
+
+        
+       makesound(event.key);
+       console.log(event.key);
+
+    });
+    
+
+   function makesound(key)
+    {
+        var audio;
+        switch (key) {
+            case 'w':
+                 audio=new Audio('sounds/tom-1.mp3');
+                break;
+
+            case 'a':
+                 audio=new Audio('sounds/tom-2.mp3');
+                break;
+
+            case 's':
+                 audio=new Audio('sounds/tom-3.mp3');
+                break;
+            
+            case 'd':
+                 audio=new Audio('sounds/tom-4.mp3');
+                break;    
+            
+            case 'j':
+                 audio=new Audio('sounds/snare.mp3');
+                break;
+                
+            case 'k':
+                 audio=new Audio('sounds/crash.mp3');
+                break;    
+   
+            case 'l':
+                 audio=new Audio('sounds/kick-bass.mp3');
+                break;    
+
+        }
+        
+        audio.play();
+    }
+
+}
+
+
