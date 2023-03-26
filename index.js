8ac2bc5229bad50cbbaf557b81bf5f5cee211da1
paraskumar09 <125036495+paraskumar09@users.noreply.github.com>
2023-03-26 21:57:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,61 +4,76 @@ for(var i=0;i<document.querySelectorAll(".drum").length;i++)
 {
     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
         
-        
-        var button= this.innerHTML;
+       var button= this.innerHTML;
        makesound(button);
-
-       
-    });
-
-
-    document.addEventListener("keydown",function(event){
-
-
-        
-       makesound(event.key);
-       console.log(event.key);
+       btnannimation(button);
 
     });
-    
-
-   function makesound(key)
-    {
-        var audio;
-        switch (key) {
-            case 'w':
-                 audio=new Audio('tom-1.mp3');
-                break;
-
-            case 'a':
-                 audio=new Audio('tom-2.mp3');
-                break;
-
-            case 's':
-                 audio=new Audio('tom-3.mp3');
-                break;
-            
-            case 'd':
-                 audio=new Audio('tom-4.mp3');
-                break;    
-            
-            case 'j':
-                 audio=new Audio('snare.mp3');
-                break;
-                
-            case 'k':
-                 audio=new Audio('crash.mp3');
-                break;    
-   
-            case 'l':
-                 audio=new Audio('kick-bass.mp3');
-                break;    
-
-        }
-        
-        audio.play();
-    }
 
 }
+
+
+document.addEventListener("keydown",function(event){
+    makesound(event.key);
+    console.log(event.key);
+
+    btnannimation(event.key);
+ });
+
+
+
+ 
+ function makesound(key)
+ {
+     var audio;
+     switch (key) {
+         case 'w':
+              audio=new Audio('sounds/tom-1.mp3');
+              
+             break;
+
+         case 'a':
+              audio=new Audio('sounds/tom-2.mp3');
+             break;
+
+         case 's':
+              audio=new Audio('sounds/tom-3.mp3');
+             break;
+         
+         case 'd':
+              audio=new Audio('sounds/tom-4.mp3');
+             break;    
+         
+         case 'j':
+              audio=new Audio('sounds/snare.mp3');
+             break;
+             
+         case 'k':
+              audio=new Audio('sounds/crash.mp3');
+             break;    
+
+         case 'l':
+              audio=new Audio('sounds/kick-bass.mp3');
+             break;    
+
+     }
+     
+     audio.play();
+ }
+
+ function btnannimation(currentkey)
+ {
+
+    var activebtn= document.querySelector("."+currentkey);
+
+        activebtn.classList.add("pressed");
+
+        setTimeout(function(){
+            activebtn.classList.remove("pressed");
+
+        },300);
+ }
+
+
 
 
